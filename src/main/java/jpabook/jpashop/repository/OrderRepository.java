fcc9295cff2f62f4dfa9da379f50b0970842d031
alexjime <alexji.dev@gmail.com>
2020-06-23 23:40:22 +0900

    create home view

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,7 +20,20 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-// OrderSearch라는 파라미터를 넘겨줄거임
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+//    // OrderSearch라는 파라미터를 넘겨줄거임
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//        // OrderSearch라는 값이 있기 때문에 가능한 정적쿼리
+//        String query = "select o from Order o join fetch o.member m" +
+//                        " where o.status = :status " +
+//                        " and m.name = :name";
+//        List<Order> resultList = em.createQuery(query, Order.class)
+//                .setParameter("status",orderSearch.getOrderStatus())
+//                .setParameter("name",orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+//        return resultList;
+//    }
 
+    // 동적쿼리 -> Querydsl을 사용할 예정
+    // public List<Order> findAll(OrderSearch orderSearch) {}
 }
