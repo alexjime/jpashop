079f1dce64d4a56c2db81fc04c394bc78370fe66
alexjime <alexji.dev@gmail.com>
2020-06-30 14:47:21 +0900

    Enroll item(book)

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class BookForm {
+
+    // 상품 수정이 있기 때문에 id값이 있어야함
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain.item;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
@@ -9,8 +11,19 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("Book")
 @Getter @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Book extends Item {
 
     private String author;
     private String isbn;
+
+//    public static Book createBook(String name, int price, int stockQuantity, String author, String isbn) {
+//        Book book = new Book();
+//        book.setName(name);
+//        book.setPrice(price);
+//        book.setStockQuantity(stockQuantity);
+//        book.setAuthor(author);
+//        book.setIsbn(isbn);
+//        return book;
+//    }
 }
